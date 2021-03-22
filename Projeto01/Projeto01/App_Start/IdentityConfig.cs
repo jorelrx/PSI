c6310fa4c58f16d12f8fa642f44d698b1a721ff9
jorelrx <49566557+jorelrx@users.noreply.github.com>
2021-03-22 18:34:24 -0300

    finish

---
+++ Projeto01/Projeto01/App_Start/IdentityConfig.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.Identity;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
+using Owin;
+using Projeto01.DAL;
+using Projeto01.Infraestrutura;
+
+namespace Projeto01.App_Start
+{
+    public class IdentityConfig
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.CreatePerOwinContext<IdentityDbContextAplicacao>(IdentityDbContextAplicacao.Create);
+            app.CreatePerOwinContext<GerenciadorUsuario>(GerenciadorUsuario.Create); 
+            app.CreatePerOwinContext<GerenciadorPapel>(GerenciadorPapel.Create);
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie, LoginPath = new PathString("/Seguranca/Account/Login"),
+            });
+        }
+    }
+}
